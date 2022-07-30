e21b76616ad50e834d5cebe2a08a5f75dc407939
sarahmtroxell <106536589+sarahmtroxell@users.noreply.github.com>
2022-07-30 14:42:21 -0400

    Add files via upload

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,7 +1,6 @@
---Challenge Code
+--Challenge Code: Deliverable 1
 
 --Create Retirement Titles Table
-
 SELECT e.emp_no
 	,e.first_name
 	,e.last_name
@@ -14,7 +13,6 @@ INNER JOIN titles AS t
 ON (e.emp_no = t.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31');
 
---Create Unique Titles Table
 -- Use Dictinct with Orderby to remove duplicate rows
 SELECT DISTINCT ON (emp_no) 
 	emp_no
@@ -26,7 +24,6 @@ FROM retirement_titles
 WHERE to_date = '9999-01-01'
 ORDER BY emp_no, to_date DESC;
 
---Create Retiring Titles Table
 --Number of Employees by Job Title to Retire
 SELECT COUNT(emp_no)
 	,title
@@ -34,3 +31,27 @@ INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY count(emp_no) DESC;
+
+--Challenge Code: Deliverable 2
+
+--Create Mentorship Elgibility Table
+SELECT DISTINCT ON (e.emp_no)
+	e.emp_no
+	,e.first_name
+	,e.last_name
+	,e.birth_date
+	,de.from_date
+	,de.to_date
+	,t.title
+INTO mentorship_elgibility
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE de.to_date = '9999-01-01'
+	AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
+
+--Challenge Code: Deliverable 3
+--Report in GitHub ReadME.md file
